ce4b87790981e35b2ba320c1da57d5865c2ca74b
Sidney Alex <silvercenturion2@gmail.com>
2020-06-12 16:17:16 -0300

    add public to access matific

--- src/routes/alunosP.js
+++ src/routes/alunosP.js
@@ -2,18 +2,33 @@ const express = require('express');
 const router = express.Router();
 
 const Alunos = require('../models/aluno');
+const MatificUser = require('../models/matific_user');
 
 router.get('/:id', async (req, res) => {
     const info = req.params;
     const { dt_nascimento } = req.body
 
-    if (!await Alunos.findOne({ _id: info.id })) return res.send({error: 'Aluno não encontrado nos registros.'})
+    if (!await Alunos.findOne({ _id: info.id })) return res.send({ error: 'Aluno não encontrado nos registros.' })
     const query = await Alunos.findOne({ _id: info.id });
 
     if (dt_nascimento == query.dt_nascimento) {
         return res.send(query);
     } else {
         return res.send({ error: "Dados não conferem" })
+    }
+
+});
+
+router.get('matific/:id', async (req, res) => {
+    const info = req.params;
+
+    try {
+        if (!await MatificUser.findOne({ _id: info.id })) return res.send({ error: 'Aluno não encontrado nos registros.' })
+        const matific = await MatificUser.findOne({ _id: info.id });
+        return res.send(matific)
+
+    } catch (error) {
+        return res.send({ error: "Aluno não encontrado" })
     }
 
 });
