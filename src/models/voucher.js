2f1cf371a5bfb9553576887b8726acb9cd6a6c65
Sidney Alex <silvercenturion2@gmail.com>
2020-10-27 15:33:56 -0300

    changed voucher mdoel

--- src/models/voucher.js
+++ src/models/voucher.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema;
 
 const Voucher = new Schema({
     _id: { type: String, unique: true, required: true },
-    login: { type: String, required: true },
-    password: { type: String, required: true },
     nome: { type: String, required: true },
     turma: { type: String, default: null },
+    email: { type: String, default: null },
+    voucher: { type: String, default: null },
     created: { type: Date, default: null },
     updated: { type: Date, default: null },
 })
