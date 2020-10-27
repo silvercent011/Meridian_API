348359129b062f117d1f539255684b1c5f22ffe7
Sidney Alex <silvercenturion2@gmail.com>
2020-10-27 15:30:05 -0300

    added voucher to public

--- src/routes/alunosP.js
+++ src/routes/alunosP.js
@@ -5,6 +5,7 @@ const Alunos = require('../models/aluno');
 const MatificUser = require('../models/matific_user');
 const InspiraUser = require('../models/inspira_user');
 const EstudaUser = require('../models/estudaDotCom');
+const VoucherUser = require('../models/voucher');
 
 router.get('/:id', async (req, res) => {
     const info = req.params;
@@ -55,6 +56,20 @@ router.get('/inspira/:id', async (req, res) => {
     try {
         if (!await InspiraUser.findOne({ _id: info.id })) return res.send({ error: 'Aluno não encontrado nos registros.' })
         const inspira = await InspiraUser.findOne({ _id: info.id });
+        return res.send(inspira)
+
+    } catch (error) {
+        return res.send({ error: "Aluno não encontrado" })
+    }
+
+});
+
+router.get('/voucher/:id', async (req, res) => {
+    const info = req.params;
+
+    try {
+        if (!await VoucherUser.findOne({ _id: info.id })) return res.send({ error: 'Aluno não encontrado nos registros.' })
+        const inspira = await VoucherUser.findOne({ _id: info.id });
         return res.send(inspira)
 
     } catch (error) {
